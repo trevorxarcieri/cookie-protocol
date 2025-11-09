52f0636e3080340e7f794afacf468120e89766c1
trevorxarcieri <agent.steel.ta@gmail.com>
2025-11-09 19:09:17 +0100

    Extend CPMsg child class forwarding while parsing.

--- src/main/java/cp/CPMsg.java
+++ src/main/java/cp/CPMsg.java
@@ -4,11 +4,9 @@ import core.Msg;
 import exceptions.IWProtocolException;
 import exceptions.IllegalMsgException;
 
-import java.util.zip.CRC32;
-import java.util.zip.Checksum;
-
 class CPMsg extends Msg {
     protected static final String CP_HEADER = "cp";
+
     @Override
     protected void create(String sentence) {
         data = CP_HEADER + " " + sentence;
@@ -18,18 +16,29 @@ class CPMsg extends Msg {
     @Override
     protected Msg parse(String sentence) throws IWProtocolException {
         CPMsg parsedMsg;
-        if(!sentence.startsWith(CP_HEADER))
+        if (!sentence.startsWith(CP_HEADER))
             throw new IllegalMsgException();
 
         String[] parts = sentence.split("\\s+", 2);
-        if(parts.length < 2)
+        if (parts.length < 2)
             throw new IllegalMsgException();
-        if(parts[1].startsWith(CPCookieRequestMsg.CP_CREQ_HEADER)) {
-            parsedMsg = new CPCookieRequestMsg();
-        } else if(parts[1].startsWith(CPCookieResponseMsg.CP_CRES_HEADER)) {
-            parsedMsg = new CPCookieResponseMsg();
-        } else {
-            throw new IllegalMsgException();
+
+        String msgType = parts[1].split("\\s+", 2)[0];
+        switch (msgType) {
+            case CPCookieRequestMsg.CP_CREQ_HEADER:
+                parsedMsg = new CPCookieRequestMsg();
+                break;
+            case CPCookieResponseMsg.CP_CRES_HEADER:
+                parsedMsg = new CPCookieResponseMsg();
+                break;
+            case CPCommandMsg.CP_CMD_HEADER:
+                parsedMsg = new CPCommandMsg();
+                break;
+            case CPCommandResponseMsg.CP_CMD_RESP_HEADER:
+                parsedMsg = new CPCommandResponseMsg();
+                break;
+            default:
+                throw new IllegalMsgException();
         }
 
         parsedMsg = (CPMsg) parsedMsg.parse(parts[1]);
