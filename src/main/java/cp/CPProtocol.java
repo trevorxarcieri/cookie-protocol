57d4640b0109e7503ee63a2916df05b0d1abca4e
trevorxarcieri <agent.steel.ta@gmail.com>
2025-11-21 14:21:24 +0100

    Create separate Cookie file so I can use it in tests.

--- src/main/java/cp/CPProtocol.java
+++ src/main/java/cp/CPProtocol.java
@@ -227,34 +227,3 @@ public class CPProtocol extends Protocol {
         throw new CookieRequestException(); // all 3 tries timed out or a req was denied, throw
     }
 }
-
-class Cookie {
-    private final long timeOfCreation;
-    private final int cookieValue;
-
-    public Cookie(long toc, int c) {
-        this.timeOfCreation = toc;
-        this.cookieValue = c;
-    }
-
-    public long getTimeOfCreation() {
-        return timeOfCreation;
-    }
-
-    public int getCookieValue() {
-        return cookieValue;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        Cookie c = (Cookie) o;
-        return ((this.timeOfCreation == c.getTimeOfCreation()) && (this.cookieValue == c.getCookieValue()));
-    }
-}
