1fe4e965c46e3c174bf46b282c7c9307529f3783
trevorxarcieri <agent.steel.ta@gmail.com>
2025-11-26 19:39:41 +0100

    Simplify command response msg by creating method to extract msg field.

--- src/main/java/cp/CPCommandResponseMsg.java
+++ src/main/java/cp/CPCommandResponseMsg.java
@@ -1,7 +1,6 @@
 package cp;
 
 import core.Msg;
-import exceptions.BadChecksumException;
 import exceptions.IWProtocolException;
 import exceptions.IllegalMsgException;
 
@@ -59,32 +58,7 @@ public class CPCommandResponseMsg extends CPMsg {
         }
         this.success = parts[2].equals("ok");
 
-        int crcDelim = parts[4].lastIndexOf(" ");
-        String msgField = (crcDelim != -1) ? parts[4].substring(0, crcDelim) : "";
-        String crcField = parts[4].substring(crcDelim + 1);
-        try {
-            int len = Integer.parseInt(parts[3]);
-            if (len < 0)
-                throw new IllegalMsgException();
-            if (len != msgField.length()) {// if length field does not match message length
-                throw new IllegalMsgException(); // message is illegal, so fail
-            }
-            if (len != 0)
-                this.data = msgField;
-        } catch (NumberFormatException e) {
-            throw new IllegalMsgException();
-        }
-
-        String[] protectedData = !msgField.isEmpty() ? new String[] { parts[0], parts[1], parts[2], parts[3], msgField }
-                : new String[] { parts[0], parts[1], parts[2], parts[3] };
-        long crc = super.getCrc(String.join(" ", protectedData)); // calculate checksum
-        try {
-            if (crc != Long.parseLong(crcField)) {
-                throw new BadChecksumException();
-            }
-        } catch (NumberFormatException e) {
-            throw new IllegalMsgException();
-        }
+        this.data = super.getMsgField(sentence, parts);
 
         return this;
     }
--- src/main/java/cp/CPMsg.java
+++ src/main/java/cp/CPMsg.java
@@ -1,6 +1,7 @@
 package cp;
 
 import core.Msg;
+import exceptions.BadChecksumException;
 import exceptions.IWProtocolException;
 import exceptions.IllegalMsgException;
 import java.util.zip.CRC32;
@@ -56,6 +57,54 @@ class CPMsg extends Msg {
         CRC32 crc32 = new CRC32();
         crc32.update(data.getBytes());
         return crc32.getValue();
+    }
+
+    /**
+     * Get the message field from the WS-delimited parts of a CPMsg, checking
+     * message length and CRC.
+     * 
+     * Such a message field is defined as a part of the CPMSg between the length
+     * field and the checksum field (separated from each by whitespace).
+     * Importantly, this means that for a command message (which has fields command
+     * and message between length and checksum), the message field will include both
+     * command and message. If the message field is empty, null is returned.
+     * 
+     * The message field must be of the length specified in the length field, and
+     * the checksum of the full CPMsg message must match the checksum field.
+     */
+    protected static String getMsgField(String fullMsg, String[] parts)
+            throws IllegalMsgException, BadChecksumException {
+        if (parts.length < 2) // at least length and msg/crc fields must be present
+            throw new IllegalMsgException();
+
+        int crcIndex = parts[parts.length - 1].lastIndexOf(" ");
+        String msgField = (crcIndex != -1) ? parts[parts.length - 1].substring(0, crcIndex).trim() : "";
+        String crcField = parts[parts.length - 1].substring(crcIndex + 1);
+
+        try {
+            int len = Integer.parseInt(parts[parts.length - 2]);
+            if (len < 0)
+                throw new IllegalMsgException();
+            if (len != msgField.length()) { // if length field does not match message length
+                throw new IllegalMsgException(); // message is illegal, so fail
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalMsgException();
+        }
+
+        long receivedCrc;
+        try {
+            receivedCrc = Long.parseLong(crcField);
+        } catch (NumberFormatException e) {
+            throw new IllegalMsgException();
+        }
+
+        long computedCrc = getCrc(fullMsg.substring(0, fullMsg.lastIndexOf(" " + crcField)));
+        if (computedCrc != receivedCrc) {
+            throw new BadChecksumException();
+        }
+
+        return msgField.isEmpty() ? null : msgField;
     }
 
 }
