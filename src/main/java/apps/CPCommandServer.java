fc1405874383d64501c094fc50ab4a4ff82b46b2
trevorxarcieri <agent.steel.ta@gmail.com>
2025-11-26 13:20:32 +0100

    Implement intial CP cmd server application.

--- src/main/java/apps/CPCommandServer.java
+++ src/main/java/apps/CPCommandServer.java
@@ -1,14 +1,39 @@
 package apps;
 
-import core.Msg;
 import cp.CPProtocol;
 import exceptions.IWProtocolException;
 import phy.PhyProtocol;
 
 import java.io.IOException;
-import java.net.InetAddress;
 
 public class CPCommandServer {
     protected static final int COMMAND_SERVER_PORT = 2000;
 
+    public static void main(String[] args) {
+        // Set up the virtual link protocol
+        PhyProtocol phy = new PhyProtocol(COMMAND_SERVER_PORT);
+
+        // Set up command protocol
+        CPProtocol cp;
+        try {
+            cp = new CPProtocol(phy, false, null, null);
+        } catch (Exception e) {
+            return;
+        }
+
+        // Start server processing
+        while (true) {
+            try {
+                String rec = cp.receive().getData();
+                if (rec != null) {
+                    System.out.println(rec);
+                }
+            } catch (IOException e) {
+                System.out.println("IO error");
+                return;
+            } catch (IWProtocolException e) {
+                System.out.println("Protocol exception: " + e.getMessage());
+            }
+        }
+    }
 }
