1439bce9e43db8f9e8290094781266220c7a3bf9
trevorxarcieri <agent.steel.ta@gmail.com>
2025-11-27 11:41:45 +0100

    Make client server name public.

--- src/main/java/apps/CPClient.java
+++ src/main/java/apps/CPClient.java
@@ -10,7 +10,7 @@ import java.io.InputStreamReader;
 import java.net.InetAddress;
 
 public class CPClient {
-    private static final String SERVER_NAME = "localhost";
+    protected static final String SERVER_NAME = "localhost";
 
     public static void main(String[] args) {
         // Each client needs to start on a unique UDP port provided by the user
